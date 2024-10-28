c496f1d6c38c87e622b07713d548b213a37558dd
marianazzk <106184155+marianazzk@users.noreply.github.com>
2024-10-28 11:35:24 -0300

    Add files via upload

---
+++ graficos/quantidadeDeUsuarios.js
@@ -0,0 +1,44 @@
+import { getCSS } from "./comum.js";
+import { tickfont } from "./comum.js";
+
+
+async function quantidadeDeUsuarios(){
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+const resultado = await fetch(url);
+const dados = await resultado.json();
+const nomeDasRedes = Object.keys(dados);
+const quantidadeDeUsuarios = Object.values(dados);
+
+const infos = [
+    {
+        x: nomeDasRedes,
+        y: quantidadeDeUsuarios,
+        type: 'bar',
+        marker: {
+            color: getCSS('--cor-primaria')
+        }
+    }
+]
+const layout = {
+    plot_bgcolor: getCSS('--cor-de-fundo'),
+    papper_bgcolor:getCSS('--cor-de-fundo'),
+    title: {
+        text: 'Redes sociais com mais usuario no mundo',
+        x: 0, 
+        font: {
+            color: getCSS('--cor-primaria'),
+            family: getCSS('--fonte'),
+            size: 30
+        }
+
+    }
+
+    
+
+}
+const grafico = document.createElement('div');
+grafico.className = 'grafico';
+document.getElementById('graficos-container').appendChild(grafico);
+Plotly.newPlot(grafico,infos)
+}
+quantidadeDeUsuarios()
